5b9d22d5c82a1f7942ec42031d751f93244875fa
Léo Schoukroun <leo@e36knots.com>
2025-10-16 23:51:54 +0900

    feat: Safe Tx POC for 'operator-registry register' command

--- src/operator.ts
+++ src/operator.ts
@@ -1,7 +1,14 @@
 import { ExtendedWalletClient, ExtendedPublicClient } from "./client";
 import { SafeSuzakuContract } from './lib/viemUtils';
 import type { Account } from "viem";
-import {logger} from './lib/logger';
+import { logger } from './lib/logger';
+import { createSafeClient } from '@safe-global/sdk-starter-kit'
+import { encodeFunctionData } from 'viem';
+
+let SIGNER_PRIVATE_KEY: string = '0x...'
+const SAFE_ADDRESS = '0x273790fE7700F4541F1c93Ed7619fdEFee47409a'
+const RPC_URL = 'https://api.avax-test.network/ext/bc/C/rpc'
+const TX_SERVICE_URL = 'https://wallet-transaction-fuji.ash.center'
 
 async function registerOperator(
     operatorRegistry: SafeSuzakuContract['OperatorRegistry'],
@@ -10,13 +17,36 @@ async function registerOperator(
 ) {
     logger.log("Registering operator...");
 
-    const hash = await operatorRegistry.safeWrite.registerOperator(
-        [metadataUrl],
-        { chain: null, account }
-    );
+    // const hash = await operatorRegistry.safeWrite.registerOperator(
+    //     [metadataUrl],
+    //     { chain: null, account }
+    // );
 
-    logger.log("Registered operator successfully, Transaction hash:", hash);
+    // logger.log("Registered operator successfully, Transaction hash:", hash);
 
+    // Create Safe client
+    const safeClient = await createSafeClient({
+        provider: RPC_URL,
+        signer: SIGNER_PRIVATE_KEY,
+        safeAddress: SAFE_ADDRESS,
+        txServiceUrl: TX_SERVICE_URL
+    })
+
+    // Prepare transaction
+    const transactions = [{
+        to: operatorRegistry.address as `0x${string}`,
+        data: encodeFunctionData({
+            abi: operatorRegistry.abi,
+            functionName: 'registerOperator',
+            args: [metadataUrl]
+        }),
+        value: '0',
+    }]
+
+    // Send transaction
+    // If threshold is 1, it will be executed immediately
+    // If threshold is greater than 1, it will be executed after the threshold is reached and tx is executed (via Safe UI)
+    const txResult = await safeClient.send({ transactions })
 }
 
 async function listOperators(
