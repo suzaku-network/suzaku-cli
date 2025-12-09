01205f127a1c982bf17bff691693393d806c5bf8
gaetan cabaret <gaetan@e36knots.com>
2025-12-09 14:39:56 +0100

    fix(commander): cleaning code

--- src/lib/commandUtils.ts
+++ src/lib/commandUtils.ts
@@ -1,28 +1,9 @@
-
-/* Use this function pattern to wrap any async action to log the result in json format
-   Use instead a proxy to propagate correct types to the callback
-export function wrapAsyncAction<T>(actionFn: (...args: T[]) => Promise<void>) {
-  return async (...args: T[]) => {
-    try {
-      await actionFn(...args);
-    } catch (error) {
-      logger.error(error);
-      process.exitCode = 1;
-    } finally {
-      await logger.printJson();
-    }
-  };
-}
-*/
 import { logger } from "./logger";
 import { Command } from "@commander-js/extra-typings";
 
 export function withJsonLogger(
   command: Command,
 ): Command {
-
-
-    // Proxy handler for safeWrite methods to simulate the write operation before executing it
     const actionHandler: ProxyHandler<Record<string, any>> = {
       get(target, prop,) {
         const fn = (target as any)[prop]
