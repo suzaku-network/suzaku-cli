aa39192c210bdc84cec0d9f4affffbb129355927
gaetan cabaret <gaetan@e36knots.com>
2026-03-09 16:52:18 +0100

    fix(client): export middleware as lib

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ export * from './lib/pChainUtils';
 export * from './lib/transferUtils';
 export * from './lib/utils';
 export * from './lib/warpUtils';
+export * from './middleware';
 
 export * from './client'
 export * from './config';
